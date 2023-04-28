f02f9ceefaf1a7c6b404088b7e46eb0480b9d744
Jennifer <jenniferbesson13@hotmail.com>
2023-04-28 10:06:57 +0200

    ajout projets à la gallery en js

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,1 +1,32 @@
+const gallery = document.querySelector(".gallery");
+
+function createElements(works) {      
+    for (let i = 0; i < works.length; i++) {
+        let work = works[i];
+        let figure = document.createElement("figure");
+        
+        let img = document.createElement("img");
+        img.src = work.imageUrl;
+        img.crossOrigin = "anonymous";
+        figure.appendChild(img);
+        
+        let figcaption = document.createElement("figcaption");
+        figcaption.innerHTML = work.title;
+        figure.appendChild(figcaption);
+
+        gallery.appendChild(figure);
+    }
+};    
+
+fetch('http://localhost:5678/api/works')
+.then(response => response.json())
+.then(json => {createElements(json)});
+
+
+
+
+function newFunction(div) {
+    div += '</figure>';
+    return div;
+}
 
