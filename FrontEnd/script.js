309172c94efe97427124f5ecb68fc7b958f28fe4
Jennifer <jenniferbesson13@hotmail.com>
2023-04-28 17:40:56 +0200

    ajout filtre portfolio

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,4 +1,7 @@
 const gallery = document.querySelector(".gallery");
+const portfolio = document.getElementById("#portfolio");
+
+
 
 function createElements(works) {      
     for (let i = 0; i < works.length; i++) {
@@ -18,7 +21,34 @@ function createElements(works) {
     }
 };    
 
+
+
+
 fetch('http://localhost:5678/api/works')
-.then(response => response.json())
-.then(json => {createElements(json)});
+.then(function (response) {
+    if (response.ok) {
+        return response.json();
+    }
+    else {
+        throw new Error('Erreur réponse de l\'API');
+    }
+})
+.then(function (works) {
+    for (let i = 0; i < works.length; i++) {
+        let work = works[i];
+        let figure = document.createElement("figure");
+        
+        let img = document.createElement("img");
+        img.src = work.imageUrl;
+        img.crossOrigin = "anonymous";
+        figure.appendChild(img);
+        
+        let figcaption = document.createElement("figcaption");
+        figcaption.innerHTML = work.title;
+        figure.appendChild(figcaption);
+
+        gallery.appendChild(figure);
+    }
+});
+
 
